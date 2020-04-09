ab9476b5f7a0b246c247260788a029aa731d2b14
chaks03 <chakshu@samagragovernance.in>
2020-04-09 09:34:02 +0530

    Fix: issues with sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       links: [
         {
-          to: 'docs/ODKFormManagementPackage',
+          to: 'docs/GettingStarted',
           activeBasePath: 'docs',
           label: 'Docs',
           position: 'left',
