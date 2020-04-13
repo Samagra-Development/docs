59d0867c61b9f3ad0f351fe195e35b5adeade45f
umangbholasamagra <63345263+umangbholasamagra@users.noreply.github.com>
2020-04-14 02:33:07 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,7 @@ module.exports = {
           'AndroidProfileManagementModule',
           'CascadingSearchModule',
           'NotificationModule',
+          'OfflineHandlingModule',
           'Grove'
         ],
       },
