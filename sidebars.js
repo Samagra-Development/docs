41a3e4d7cae56b5274075a8eab0fbebd2a7f63dc
umangbholasamagra <63345263+umangbholasamagra@users.noreply.github.com>
2020-04-14 00:24:33 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -20,6 +20,7 @@ module.exports = {
           'COMobileApplication',
           'FormManagementPackage',
           'AndroidProfileManagementModule',
+          'CascadingSearchModule',
           'NotificationModule',
           'Grove'
         ],
