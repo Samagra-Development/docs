cfdee5c8de229c3b4df015a991d0497630bdb3da
sukhpreetsamagra <46666190+sukhpreetsamagra@users.noreply.github.com>
2020-04-20 10:16:36 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -28,7 +28,9 @@ module.exports = {
           'NotificationModule',
           'OfflineHandlingModule',
           'Grove',
-          'CommunicatingAmongModules'
+          'References': [
+            'CommunicatingAmongModules'
+            ],
         ],
       },
       {
