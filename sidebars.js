1e4409120531c90f662b2d61a04f6d03f09c3d7a
umangbholasamagra <63345263+umangbholasamagra@users.noreply.github.com>
2020-04-20 10:09:52 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -20,7 +20,7 @@ module.exports = {
       {
         'Mobile Application': [
           'COMobileApplication',
-          'GettingStarted',
+          'GettingStartedMobileComponent',
           'FormManagementModule',
           'AncillaryScreensModule',
           'AndroidProfileManagementModule',
