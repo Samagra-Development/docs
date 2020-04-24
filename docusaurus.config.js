2b2a74035f0653399741276432f0aaeec0b529fd
chaks03 <chakshu@samagragovernance.in>
2020-04-24 08:39:46 +0530

    Fix: Blog config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        // {to: 'blog', label: 'Blog', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
         // Please keep GitHub link to the right for consistency.
         {
           href: 'https://Samagra-Development.github.io/docs',
