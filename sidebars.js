c192b749353a555c158dc7f3f1be6200b1958705
sukhpreetsamagra <46666190+sukhpreetsamagra@users.noreply.github.com>
2020-04-25 11:01:12 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -34,7 +34,7 @@ module.exports = {
       {
         'Application Control': [
           'COAppControl',
-          'AdminGettingStarted',
+          'AppControlPanel',
           'AdminAddingExternalData',
           'AdminAuthentication',
           'AdminAuthorisation',
